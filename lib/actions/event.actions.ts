27369172bcda41be214cef97fd7cfcb1316c2340
venkatsuhas <47750299+venkatsuhas@users.noreply.github.com>
2026-05-02 00:47:56 +0530

    book event using server actions (#9)

---
+++ lib/actions/event.actions.ts
@@ -0,0 +1,16 @@
+'use server';
+
+import Event from '@/database/event.model';
+import {connectToDatabase} from "../mongodb";
+
+export const getSimilarEventsBySlug = async (slug: string) => {
+    try {
+        await connectToDatabase();
+
+        const event = await Event.findOne({ slug });
+
+        return await Event.find({ _id: { $ne: event._id }, tags: { $in: event.tags } }).lean();
+    } catch {
+        return [];
+    }
+}
