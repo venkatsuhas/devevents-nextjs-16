f0e263edbf274b57434a9766642b493985bc67d5
suhasnoonepalli <kamalsuhas7@gmail.com>
2026-07-08 00:46:24 +0530

    added changes

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Layout = ({children}: {children:React.ReactNode}) => {
     return (
         <div>
-            Dashboard Layout
+            Dashboard Layout!!
             {children}
         </div>
     )
