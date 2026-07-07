e0bbabc91f511ffcc33996a78d9413a47f8a16e9
suhasnoonepalli <kamalsuhas7@gmail.com>
2026-07-08 01:13:44 +0530

    testing 2

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Dashboard = () => {
     // throw new Error("Dashboard Error")
     return (
-        <div>Dashboard</div>
+        <div>Dashboard Welcome</div>
     )
 }
 export default Dashboard
